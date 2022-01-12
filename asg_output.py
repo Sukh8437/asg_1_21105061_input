8210d6346d2a539044aa8faa375927f83e58aca7
Sukh8437 <97590005+Sukh8437@users.noreply.github.com>
2022-01-12 18:11:40 +0530

    Add files via upload

---
+++ asg_output.py
@@ -0,0 +1,32 @@
+#first program
+
+enter first number:
+enter second number:
+enter third number:
+avg is:
+
+
+#second program
+
+enter the gross income:
+enter no of dependents:
+income tax:
+
+
+#third program
+
+student= [SID,Name,Gender,Couse Name,CGPA]
+student=[21105061, 'Sukhwinder', 'F', 'ECE', 9.8]
+
+
+#fourth program
+
+Marks of five students
+list before sorting:
+list after sorting:
+
+
+#fiffth program
+
+['Red', 'Green', 'white', 'pink', 'yellow']
+['Red', 'Green', 'white', 'purple', 'yellow']
